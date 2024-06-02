c3dfa1cd4ca27b464a475225cd0147024965fe9f
patroxfe <patryksulich42@gmail.com>
2024-06-02 11:45:14 +0200

    Dodano ikonki do sekcji kontakt

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,27 +1,27 @@
 import { Link } from 'react-router-dom'
+import Phone from '../assets/phone-contact.svg'
+import Email from '../assets/email-contact.svg'
+import Location from '../assets/location-contact.svg'
 
-export default function Contanct() {
-	const getCurrentDate = () => {
-		const today = new Date()
-		const day = String(today.getDate()).padStart(2, '0')
-		const month = String(today.getMonth() + 1).padStart(2, '0') // Styczeń jest miesiącem 0!
-		const year = today.getFullYear()
-
-		return `${day}-${month}-${year}`
-	}
-
+export default function Contact() {
 	return (
 		<>
-			<section className='pt-20 pb-10 lg:pb-0 bg-[#111111]' id='kontakt'>
+			<section className='pt-20 pb-10 lg:pb-0 bg-[#151515]' id='kontakt'>
 				<div className='gap-5 wrapper flex flex-col text-center lg:text-left tracking-wider md:grid md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 lg:grid-rows-2 text-[#dbdbdb] lg:space-x-8 px-10 xl:px-0 space-y-6 md:space-y-0'>
-					<div className=' font-light'>
-						<p className='py-1 text-2xl font-semibold mb-2'>E-mail</p>
+					<div className='font-light text-center lg:text-left'>
+						<div className='flex items-center justify-center lg:justify-start mb-2'>
+							<img src={Email} alt='Email Icon' className='w-6 h-6 mr-3' />
+							<p className='text-2xl font-semibold'>E-mail</p>
+						</div>
 						<p className='py-1 text-lg'>emarika@gmail.com</p>
-						<p className='py-1  text-lg'>rafaldruzba@op.pl</p>
-						<p className='py-1 center text-lg'>patryksulich42@gmail.com</p>
+						<p className='py-1 text-lg'>rafaldruzba@op.pl</p>
+						<p className='py-1 text-lg'>patryksulich42@gmail.com</p>
 					</div>
-					<div className=' font-light flex flex-col '>
-						<p className='text-2xl font-semibold mb-2'> Telefon</p>
+					<div className='font-light flex flex-col'>
+						<div className='flex items-center justify-center lg:justify-start mb-2'>
+							<img src={Phone} alt='Phone Icon' className='w-6 h-6 mr-3' />
+							<p className='text-2xl font-semibold'>Telefon</p>
+						</div>
 						<a href='tel:+48791445104' className='cursor-pointer hover:text-blueMain text-lg py-1'>
 							+48 791 445 104
 						</a>
@@ -29,33 +29,37 @@ export default function Contanct() {
 							+48 662 131 525
 						</a>
 					</div>
-					<div className=' lg:px-5 font-light'>
-						<p className='text-2xl font-semibold mb-2'>Lokalizacja</p>
+					<div className='lg:px-5 font-light'>
+						<div className='flex items-center justify-center lg:justify-start mb-2'>
+							<img src={Location} alt='Location Icon' className='w-6 h-6 mr-3' />
+							<p className='text-2xl font-semibold'>Lokalizacja</p>
+						</div>
 						<p className='text-lg py-1'>07-130 Łochów,</p>
-						<p className='text-lg py-1'> Sadowa 9</p>
+						<p className='text-lg py-1'>Sadowa 9</p>
 					</div>
-					<div className='flex flex-col  font-light'>
+					<div className='flex flex-col font-light'>
 						<p className='text-2xl font-semibold mb-2'>Prawne</p>
 						<Link to='/politykaprywatnosci' className='cursor-pointer hover:text-blueMain text-lg py-2'>
 							Polityka prywatności
 						</Link>
-						<Link to='/regulamin' className=' cursor-pointer hover:text-blueMain text-lg py-2'>
+						<Link to='/regulamin' className='cursor-pointer hover:text-blueMain text-lg py-2'>
 							Regulamin
 						</Link>
 					</div>
-					<div className='flex flex-col text-center col-span-2 lg:col-span-4  lg:mb-16'>
-						<h3 className='text-4xl lg:text-4xl font-semibold pb-1 '>
-							{' '}
+					<div className='flex flex-col text-center col-span-2 lg:col-span-4 lg:mb-16'>
+						<h3 className='text-4xl lg:text-4xl font-semibold pb-1'>
 							<span className='text-secondBlue'>e</span>Marika.pl
 						</h3>
 						<p className='text-[#dbdbdb]'>
-							Jesteśmy na rynku od <span className=' font-medium'>ponad 20 lat</span>!
+							Jesteśmy na rynku od <span className='font-medium'>ponad 20 lat</span>!
 						</p>
 					</div>
 				</div>
 			</section>
 			<footer className='bg-[#000000e1] flex flex-col text-center text-[#dbdbdb] p-4'>
-				<p className=''><span className='font-bold'>eMarika.pl &copy;</span> | Projektowanie Stron WWW </p>
+				<p>
+					<span className='font-bold'>eMarika.pl &copy;</span> | Projektowanie Stron WWW
+				</p>
 			</footer>
 		</>
 	)
