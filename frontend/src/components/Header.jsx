2b95ad25eb2a3b16b31ce57fbc6a295e3431e3f4
patroxfe <patryksulich42@gmail.com>
2024-06-09 14:14:40 +0200

    dodano reveal-scrolla na headerze (do poprawy) i w summary

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,56 @@
-import { scrollToSection } from '../utils/scrollUtils' // Importujemy funkcję scrollToSection
+import { motion } from 'framer-motion';
+import { scrollToSection } from '../utils/scrollUtils';
 
 export default function Header() {
 	const handleSprawdzOferteClick = () => {
-		scrollToSection('cennik-i-oferty') // Wywołujemy funkcję scrollToSection z scrollUtils.js
-	}
+		scrollToSection('cennik-i-oferty');
+	};
+
+	const containerVariants = {
+		hidden: {},
+		visible: {
+			transition: {
+				staggerChildren: 0.3,
+			},
+		},
+	};
+
+	const itemVariants = {
+		hidden: { opacity: 0, y: 20 },
+		visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+	};
 
 	return (
-		<>
-			<section className='text-center lg:text-left lg:flex bg-[#1b2841ff] relative overflow-hidden py-24 px-0 h-screen flex justify-center items-center' id='header'>
-				<div className='wrapper flex items-center justify-center text-center lg:gap-x-12 z-10 relative px-6 sm:px-10 md:px-14'>
-					<div className='lg:min-w-[30%] lg:max-w-[50%]'>
-						<h1 className='font-bold text-3xl pt-[130px] leading-10 max-w-3xl md:text-4xl lg:text-5xl'>
-							Stworzymy Ci wymarzoną stronę internetową
-						</h1>
-						<div className='py-10 space-y-1'>
-							<p className='text-lg md:text-xl leading-6 font-light text-white'>Szybko zrealizujemy twoje zlecenie</p>
-							<p className='text-lg md:text-xl leading-6 font-light text-white'>Użyjemy najnowszych narzędzi</p>
-						</div>
-						<button
-							className='bg-white drop-shadow-lg text-[#1b2841] text-lg font-semibold py-2 px-5 rounded mb-[100px] hover:bg-blueMain transition duration-300'
-							onClick={handleSprawdzOferteClick}>
-							Sprawdź ofertę!
-						</button>
-					</div>
+		<motion.section
+			className='text-center lg:text-left lg:flex bg-[#1b2841ff] relative overflow-hidden py-24 px-0 h-screen flex justify-center items-center'
+			id='header'
+			initial='hidden'
+			animate='visible'
+			variants={containerVariants}
+		>
+			<div className='wrapper flex items-center justify-center text-center lg:gap-x-12 z-10 relative px-6 sm:px-10 md:px-14'>
+				<div className='lg:min-w-[30%] lg:max-w-[50%]'>
+					<motion.h1
+						className='font-bold text-3xl pt-[130px] leading-10 max-w-3xl md:text-4xl lg:text-5xl'
+						variants={itemVariants}
+					>
+						Stworzymy Ci wymarzoną stronę internetową
+					</motion.h1>
+					<motion.div className='py-10 space-y-1' variants={itemVariants}>
+						<p className='text-lg md:text-xl leading-6 font-light text-white'>Szybko zrealizujemy twoje zlecenie</p>
+						<p className='text-lg md:text-xl leading-6 font-light text-white'>Użyjemy najnowszych narzędzi</p>
+					</motion.div>
+					<motion.button
+						className='bg-white drop-shadow-lg text-[#1b2841] text-lg font-semibold py-2 px-5 rounded mb-[100px] hover:bg-blueMain transition duration-300'
+						onClick={handleSprawdzOferteClick}
+						variants={itemVariants}
+					>
+						Sprawdź ofertę!
+					</motion.button>
 				</div>
-				<div className='hero-img hero-img-desktop hidden md:block'></div>
-				<div className='hero-img hero-img-mobile block md:hidden'></div>
-			</section>
-		</>
-	)
+			</div>
+			<div className='hero-img hero-img-desktop hidden md:block'></div>
+			<div className='hero-img hero-img-mobile block md:hidden'></div>
+		</motion.section>
+	);
 }
--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -1,69 +1,92 @@
-import SummaryImg from '../assets/summary-img.svg'
-import CrownImg from '../assets/mdi_crown.svg'
-import MobileImg from '../assets/circum_mobile.svg'
-import PriceImg from '../assets/circum_price.svg'
-import TimeImg from '../assets/circum_time.svg'
+import { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import SummaryImg from '../assets/summary-img.svg';
+import CrownImg from '../assets/mdi_crown.svg';
+import MobileImg from '../assets/circum_mobile.svg';
+import PriceImg from '../assets/circum_price.svg';
+import TimeImg from '../assets/circum_time.svg';
 
 export default function Summary() {
-	const questions = [
-		{
-			title: 'Responsywność',
-			description:
-				'Nasze strony internetowe są responsywne i kompatybilne z każdym urządzeniem. W związku z tym twoi potencjalni klienci nie będą mieli problemu z odwiedzaniem twojej witryny!',
-			fade: 'from-firstViolet to-secondViolet',
-			icon: MobileImg,
-		},
-		{
-			title: 'Atrakcyjne ceny',
-			description:
-				'Staramy się połączyć jakość z kosztem wykonania. Jesteśmy jedną z najlepszych opcji na polskim rynku! Nasze ceny są nieporównywalnie niższe niż u konkurencji.',
-			fade: 'from-firstGreen to-secondGreen',
-			icon: PriceImg,
-		},
-		{
-			title: 'Szybkie terminy',
-			description:
-				'Zależy ci na czasie? Jesteśmy w stanie sprostać twoim oczekiwaniom. Z nami nie martwisz się o długi czas oczekiwania na stronę...',
-			fade: 'from-firstRed to-secondRed',
-			icon: TimeImg,
-		},
-		{
-			title: 'Gwarancja',
-			description:
-				'Gwarantujemy bezproblemowość i możliwości ewentualnych bezpłatnych zmian w razie różnorodnych problemów. Nie musisz się martwić o przyszłość, załatwimy to!',
-			fade: 'from-firstBlue to-secondBlue',
-			icon: CrownImg,
-		},
-	]
+    const questions = [
+        {
+            title: 'Responsywność',
+            description: 'Nasze strony internetowe są responsywne i kompatybilne z każdym urządzeniem. W związku z tym twoi potencjalni klienci nie będą mieli problemu z odwiedzaniem twojej witryny!',
+            fade: 'from-firstViolet to-secondViolet',
+            icon: MobileImg,
+        },
+        {
+            title: 'Atrakcyjne ceny',
+            description: 'Staramy się połączyć jakość z kosztem wykonania. Jesteśmy jedną z najlepszych opcji na polskim rynku! Nasze ceny są nieporównywalnie niższe niż u konkurencji.',
+            fade: 'from-firstGreen to-secondGreen',
+            icon: PriceImg,
+        },
+        {
+            title: 'Szybkie terminy',
+            description: 'Zależy ci na czasie? Jesteśmy w stanie sprostać twoim oczekiwaniom. Z nami nie martwisz się o długi czas oczekiwania na stronę...',
+            fade: 'from-firstRed to-secondRed',
+            icon: TimeImg,
+        },
+        {
+            title: 'Gwarancja',
+            description: 'Gwarantujemy bezproblemowość i możliwości ewentualnych bezpłatnych zmian w razie różnorodnych problemów. Nie musisz się martwić o przyszłość, załatwimy to!',
+            fade: 'from-firstBlue to-secondBlue',
+            icon: CrownImg,
+        },
+    ];
 
-	return (
-		<>
-			<section className='bg-white py-10 lg:flex lg:flex-row-reverse lg:items-center lg:justify-center lg:py-16'>
-				<div className='wrapper flex flex-col items-center lg:gap-x-24 justify-center px-6 sm:px-10 md:px-14 2xl:px-0 max-w-2xl lg:flex-row '>
-					<div className='lg:order-2 lg:w-1/2 md:max-w-2xl space-y-8'>
-						{questions.map((item, index) => (
-							<div key={index}>
-								<div className={`[box-shadow:_0px_0px_13px_rgb(0_0_0_/_15%)] rounded flex p-5 ${item.fade}`}>
-									<div className='flex items-center justify-center bg-gradient-to-r w-14 h-14 mr-5 rounded-full flex-shrink-0'>
-										<img src={item.icon} alt='icon' className='w-9 h-9' />
-									</div>
-									<div className='flex flex-col'>
-										<h2 className='text-mainText font-semibold text-lg lg:text-xl'>{item.title}</h2>
-										<p className='text-sm text-thirdText lg:text-base lg:pt-[2px]'>{item.description}</p>
-									</div>
-								</div>
-							</div>
-						))}
-					</div>
-					<div className='lg:w-1/2'>
-						<img
-							className='w-full lg:max-w-2xl my-6 lg:my-0'
-							src={SummaryImg}
-							alt='Obrazek pomagający zilustrować opisane podpunkty, pomaga w wyglądzie strony'
-						/>
-					</div>
-				</div>
-			</section>
-		</>
-	)
+    return (
+        <>
+            <section className='bg-white py-10 lg:flex lg:flex-row-reverse lg:items-center lg:justify-center lg:py-16'>
+                <div className='wrapper flex flex-col items-center lg:gap-x-24 justify-center px-6 sm:px-10 md:px-14 2xl:px-0 max-w-2xl lg:flex-row '>
+                    <div className='lg:order-2 lg:w-1/2 md:max-w-2xl space-y-8'>
+                        {questions.map((item, index) => (
+                            <QuestionBlock key={index} item={item} />
+                        ))}
+                    </div>
+                    <div className='lg:w-1/2'>
+                        <img
+                            className='w-full lg:max-w-2xl my-6 lg:my-0'
+                            src={SummaryImg}
+                            alt='Obrazek pomagający zilustrować opisane podpunkty, pomaga w wyglądzie strony'
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+function QuestionBlock({ item, }) {
+    const controls = useAnimation();
+    const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible');
+        }
+    }, [controls, inView]);
+
+    const variants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+    };
+
+    return (
+        <motion.div
+            ref={ref}
+            initial='hidden'
+            animate={controls}
+            variants={variants}
+            className={`[box-shadow:_0px_0px_13px_rgb(0_0_0_/_15%)] rounded flex p-5 ${item.fade}`}
+        >
+            <div className='flex items-center justify-center bg-gradient-to-r w-14 h-14 mr-5 rounded-full flex-shrink-0'>
+                <img src={item.icon} alt='icon' className='w-10 h-10 animated-icon' />
+            </div>
+            <div className='flex flex-col'>
+                <h2 className='text-mainText font-semibold text-lg lg:text-xl'>{item.title}</h2>
+                <p className='text-sm text-thirdText lg:text-base lg:pt-[2px]'>{item.description}</p>
+            </div>
+        </motion.div>
+    );
 }
